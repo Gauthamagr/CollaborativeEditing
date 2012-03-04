ad2dab0da961d1eb64afd7647121c90a3fc75889
Pushkar Gopalakrishna <pushkar87_dg@yahoo.co.in>
2012-03-04 15:38:51 -0800

    Completed the initial connection setup function

--- server/DocumentServer.java
+++ server/DocumentServer.java
@@ -9,27 +9,48 @@ public class DocumentServer {
 
 	//This is required to send the entire Html
 	public void establishInitialConnection() throws FileNotFoundException , IOException{
+		String line_in_file, full_html = "";
 		FileReader html_file = new FileReader("page.html"); 
 		BufferedReader html_file_br = new BufferedReader(html_file);
 
-		String line_in_file, full_html = "";
 		while( (line_in_file=html_file_br.readLine() ) != null){
-			full_html+=line_in_file;
+			full_html=  full_html + line_in_file + "\n";
 		}
 
 		System.out.println("Waiting for connection ");
-        ServerSocket Server = new ServerSocket (SERVER_PORT);
+
+        ServerSocket Server;
+		try{
+        	Server = new ServerSocket (SERVER_PORT);
+			Server.setReuseAddress(true);
+		}catch(Exception e){
+			System.out.println("Issue with close connection ?? !!!!!!!!! " + e.toString() +"\n\n" );
+			//System.out.println("Cause : " + e.getCause() );
+			return;
+		}
+
         Socket connection = Server.accept();
+		connection.setSoLinger(false, 0);
 		PrintWriter outToClient = new PrintWriter(connection.getOutputStream(),true);
 		String header="HTTP/1.1: 200 OK\r\n"+"Content-Type: text/html\r\n" + "\r\n";
  		outToClient.println(header +  full_html);
+		connection.shutdownOutput();
+		outToClient.close();
 		connection.close();
+		if(!connection.isClosed() ) {
+			System.out.println("Cannot close the connection and create another 1 ");
+			return;
+		}
+
 		System.out.println("Sent the html ");
+		Server.close();
+		return;
 	}
 
 	public void establishPersistentConnection() throws  FileNotFoundException , IOException {
 
         ServerSocket Server = new ServerSocket (SERVER_PORT);
+		System.out.println("Connect button clocked ");
 		for(int i =0;i<this.number_of_clients ;i++){
         	Socket clientSocket = Server.accept();
 			//String persistent_header="HTTP/1.1: 200 OK\r\n"+"Content-Type: text/html\r\n" + "Connection:Keep-Alive\r\n" + "\r\n";
@@ -46,10 +67,14 @@ public class DocumentServer {
 		for(int i = 0;i<this.number_of_clients;i++){
 			try{
 				establishInitialConnection();
+				Thread.sleep(1000);
 			}catch(FileNotFoundException e){
-				System.out.println("Cannot establish initial connection");
+				System.out.println("Cannot establish initial connection. FileNotFoundException");
 			}catch(IOException e){
-				System.out.println("Cannot establish initial connection!");
+				System.out.println("Cannot establish initial connection. IOException" + e.toString());
+			}catch(InterruptedException e){
+				System.out.println("Cannot establish initial connection. InterruptedException");
+				
 			}
 		}
 
@@ -57,9 +82,9 @@ public class DocumentServer {
 		try{
 			establishPersistentConnection();
 		}catch(FileNotFoundException e){
-			System.out.println("Could not establish persistent connection");
+			System.out.println("Could not establish persistent connection. FileNotFoundException");
 		}catch(IOException e){
-			System.out.println("Could not establish persistent connection");
+			System.out.println("Could not establish persistent connection. IOException");
 		}
 
 	}
