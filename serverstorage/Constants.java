4a9c9d8c8b3eab5cd860ea740d36e5bc9b9145f3
Gautham Reddy <gautham.agr@gmail.com>
2012-03-13 22:31:59 -0700

    change in packet communication

--- serverstorage/Constants.java
+++ serverstorage/Constants.java
@@ -1,7 +1,7 @@
 package serverstorage;
 
 public class Constants {
-	public final static int BACKSPACE = 'B';
-	public final static int DELETE = 24;
+	public final static int BACKSPACE = 'X';
+	public final static int DELETE = 'Y';
 	
 }
