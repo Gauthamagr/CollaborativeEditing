c34937392162d0b15a67a1aca7d7db0b37dd702b
Gautham Reddy <gautham.agr@gmail.com>
2012-03-31 00:57:03 -0700

    removing duplicates due to network issues

--- serverstorage/HistoryElement.java
+++ serverstorage/HistoryElement.java
@@ -3,6 +3,7 @@ package serverstorage;
 public class HistoryElement {
 	int client_id;
 	int version_number;
+	int client_version_number; // this was handle to eliminate duplicates due to network issues
 	char character_pressed;
 	int position;
 	public int getClient_id() {
@@ -17,6 +18,13 @@ public class HistoryElement {
 	public void setVersion_number(int version_number) {
 		this.version_number = version_number;
 	}
+	public int getClient_version_number() {
+		return client_version_number;
+	}
+	public void setClient_version_number(int client_version_number) {
+		this.client_version_number = client_version_number;
+	}
+	
 	public char getCharacter_pressed() {
 		return character_pressed;
 	}
